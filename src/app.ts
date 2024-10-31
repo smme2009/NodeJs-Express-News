2c12948cc9c485f4f77b9124a49c405f6a8271d3
smme2009 <smme2009@gmail.com>
2024-10-31 16:35:09 +0800

    feat: 增加基本路由

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import env from "dotenv";
+import router from "@/router/index";
 import sequelize from "@/init/sequelize";
 
 // 初始化env
@@ -8,6 +9,10 @@ env.config();
 const app = express();
 const port = 3000;
 
+// 加入路由
+app.use(router);
+
+// 初始化資料庫套件
 sequelize.sync();
 
 app.listen(port);
