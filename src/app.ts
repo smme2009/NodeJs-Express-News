4cf4b2a49378da9df17e89efc3418d2253547de8
smme2009 <smme2009@gmail.com>
2024-10-31 16:12:12 +0800

    feat: 增加Sequelize套件初始化機制

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import env from "dotenv";
+import sequelize from "@/init/sequelize";
 
 // 初始化env
 env.config();
 
 const app = express();
 const port = 3000;
+
+sequelize.sync();
 
 app.listen(port);
