a4b295dd9074e090c09f2dbff3c91a02b3281310
smme2009 <smme2009@gmail.com>
2024-11-03 17:36:53 +0800

    feat: 新增分頁env參數

--- src/type/system/env.ts
+++ src/type/system/env.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
     interface ProcessEnv {
+        // APP
+        PAGE_SIZE: string;
+
+        // DB
         DB_HOST: string;
         DB_PORT: string;
         DB_DATABASE: string;
