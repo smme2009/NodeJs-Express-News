4994d942b209f5b7fec4d6485946a05bccd47c89
smme2009 <smme2009@gmail.com>
2024-11-15 17:49:33 +0800

    feat: 調整資料庫初始化的機制

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 import express from "express";
 import env from "dotenv";
 import router from "@/router/index";
-import sequelize from "@/init/sequelize";
+import { Sequelize } from "sequelize-typescript";
+
+// 初始化框架
+const app = express();
 
 // 初始化env
 env.config();
+env.config({ path: `.env.${process.env.NODE_ENV}` });
 
-const app = express();
-const port = 3000;
+// 初始化資料庫
+const sequelize: Sequelize = new Sequelize(
+    process.env.DB_DATABASE ?? "",
+    process.env.DB_USERNAME ?? "",
+    process.env.DB_PASSWORD ?? "",
+    {
+        host: process.env.DB_HOST ?? "",
+        dialect: "mysql",
+    }
+);
+
+// 加入Model
+sequelize.addModels([__dirname + "/database/model"]);
 
 // 加入路由
 app.use(router);
 
-// 初始化資料庫套件
-sequelize.sync();
-
-app.listen(port);
+// 開始監聽
+app.listen(process.env.APP_PORT);
