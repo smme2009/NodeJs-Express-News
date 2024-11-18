84370858b027809ba63540e5c309f4879de8f02d
smme2009 <smme2009@gmail.com>
2024-11-18 17:04:47 +0800

    feat: 移除棄用檔案

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import rtProduct from "@/router/shop/product";
 import RtLogin from "@/router/mgmt/login";
 
 const router: Router = Router();
 
-router.use("/api/shop", rtProduct);
 router.use("/api/mgmt", RtLogin);
 
 export default router;
--- src/type/request/shop/product/search.ts
+++
@@ -1,4 +0,0 @@
-export default interface Search {
-    keyword: string;
-    productTypeId: string;
-}
