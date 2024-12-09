8aee4b2ce3785c506bb1cfcdcd77a4e3ebb5dd21
smme2009 <smme2009@gmail.com>
2024-12-09 14:38:48 +0800

    feat: 新增全域的專案路徑相關變數

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Express from "express";
+import Path from "path";
 import BodyParser from "body-parser";
 import Env from "dotenv";
 import RtBackend from "@/router/backend/index";
@@ -7,6 +8,11 @@ import { Sequelize } from "sequelize-typescript";
 
 // 初始化框架
 const app = Express();
+
+// 取得路徑，該機制在專案結構發生變更時，需視情況進行調整
+global.appPath = __dirname; // 取得app.js(ts)路徑
+global.basePath = Path.join(global.appPath, ".."); // 取得專案路徑
+global.storagePath = Path.join(global.basePath, "storage"); // storage路徑
 
 // 初始化env
 Env.config();
---
+++ src/type/auto/global.d.ts
@@ -0,0 +1,7 @@
+declare global {
+    var appPath: string;
+    var basePath: string;
+    var storagePath: string;
+}
+
+export {};
