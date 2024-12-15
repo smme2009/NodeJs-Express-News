18af0a6d51117f5edd9bbb6009809b14f1e05f46
smme2009 <smme2009@gmail.com>
2024-12-15 15:42:35 +0800

    feat: 調整env模式的機制

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 
-// 根據env模式產生指定con
 module.exports = {
-	[process.env.NODE_ENV]: {
+	'development': {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+		migrationStorageTableName: 'migration',
+	},
+	'production': {
 		username: process.env.DB_USERNAME,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE,
